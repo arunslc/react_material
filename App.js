1c1b7cc96f060004c70aee8025b5456ee73eecc9
arunslc <72987859+arunslc@users.noreply.github.com>
2021-11-18 22:17:32 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
-import { Grid, makeStyles } from "@material-ui/core";
-import Feed from "./components/Feed";
-import Leftbar from "./components/Leftbar";
-import Navbar from "./components/Navbar";
-import Rightbar from "./components/Rightbar";
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
 
-
-const useStyles = makeStyles((theme) => ({
-  right:{
-    [theme.breakpoints.down("sm")]:{
-      display:"none",
-    },
-  },
-}));
-
-const App = () => {
-    const classes = useStyles();
+function App() {
     return ( 
-    <div>
-        <Navbar />
-         <Grid container>
-            <Grid item sm = { 2 } xs={2}>
-                <Leftbar />
-              </Grid>  
-            <Grid item sm = { 7 } xs={10}>
-              <Feed />
-            </Grid>  
-            <Grid item sm = { 3 } className={classes.right}>
-               <Rightbar />
-            </Grid>  
-          </Grid> 
-        </div> 
+    <div className = "App" >
+        <Login />
+    </div>
     );
-};
+}
 
 export default App;
